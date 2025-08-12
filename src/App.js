9d7a1e4a9fe42aac8a74d309e5be8eed276ec385
Alex Coelho <alexcastelo437@mail.com>
2025-08-12 09:01:36 -0300

    feat: adicionando cors e config geral do manipulador de erros em app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
  import "../src/model/Index.js"
 import express from "express"
+import cors from "cors"
 
 import indexRouter from "../src/routes/Index.js"
 import { inicializar } from "./config/database/Index.js"
+import errorHandler from "./utils/errors/ErrorHandler.js"
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use("/api/v1", indexRouter)
+app.use(errorHandler)
 
 inicializar()
 
